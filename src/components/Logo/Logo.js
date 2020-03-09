c723e16f4699f0a6039cfb5286035f20ea944960
ross <twnnxt@gmail.com>
2020-03-09 15:50:05 +0800

    3c

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,7 +7,7 @@ import webLogo from '../../assets/images/logo.png';
 const Logo = props => (
     <Navbar.Brand>
         <Link to="/">
-            <img src={webLogo} alt="logo" />
+            <img src={webLogo} alt="logo" style={{width: '50px', height: '50px'}}/>
         </Link>
     </Navbar.Brand>
 
